032e733bf44d74ec31f94de4945e16f6b9c642a7
Ran <razaanarjuna@gmail.com>
2025-08-17 07:32:32 +0700

    Add flower planting mechanic and assets
    Introduced a new flower planting feature: the player can now plant a flower by pressing Space if they have seeds. Added Flower.png asset and its import, created flower.tscn scene, updated player.gd and player.tscn to support flower instantiation, and modified level.tscn to group seeds under a new 'Seeds' node. Also updated icon.svg to a new 64x64 design and increased the default window size in project.godot.

--- Codes/player.gd
+++ Codes/player.gd
@@ -3,12 +3,17 @@ extends CharacterBody2D
 @export var moveSpeed : float = 150
 var moveDirection : Vector2
 var seed_count : int = 0
-
+@export var flower_scene : PackedScene
 @onready var animated_sprite_2d = $AnimatedSprite2D
 
 
 # Map Size
 # 12 x 18 Tiles
+
+func _process(delta):
+	if Input.is_action_just_pressed("Space") and seed_count > 0:
+		plant_seed()
+
 
 func _physics_process(delta):
 	moveDirection.x = Input.get_axis("Left", "Right")
@@ -33,3 +38,21 @@ func _physics_process(delta):
 func add_seed():
 	seed_count += 1
 	print("Seeds: ", seed_count)
+
+
+func plant_seed():
+	var flower = flower_scene.instantiate()
+	get_parent().add_child(flower)
+	if animated_sprite_2d.flip_h:
+		# Create animation of bloom
+		flower.global_position = global_position + Vector2(16, 0) # Plant where player is standing
+	else:
+		# Create animation of bloom
+		flower.global_position = global_position + Vector2(-16, 0) # Plant where player is standing 
+	
+	seed_count -= 1
+	print("Planted! Seeds left: ", seed_count)
+	
+	await get_tree().create_timer(1.0).timeout
+	# Create animation of it to go
+	flower.queue_free()
